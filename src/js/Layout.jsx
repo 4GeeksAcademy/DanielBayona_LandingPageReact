f550faca0735c41512546d6eb79eb1dc2ca2da43
Bayunas <bayunas12@gmail.com>
2024-06-19 14:38:56 -0500

    Fixed path fro NavBar.jsx

--- src/js/Layout.jsx
+++ src/js/Layout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import NavBar from "./components/navBar/NavBar.jsx";
+import NavBar from "./components/NavBar/NavBar.jsx";
 import Home from "./Views/Home.jsx";
 import Footer from "./components/Footer/Footer.jsx"
 
